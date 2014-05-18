d983b3fa1d55b171a908ae6af538d771762f936d
Bairon <baiper06@gmail.com>
2014-05-17 18:56:27 -0600

    Cambio a Mobile Angular UI

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,80 @@
-var angulargap = angular.module("angulargap", []);
-angulargap.controller("HomeController", function ($scope) {
-    $scope.message = "AngularJS!";
+var app = angular.module('Apparta', [
+  "ngRoute",
+  "ngTouch",
+  "mobile-angular-ui"
+]);
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider.when('/',          {templateUrl: "partials/home.html"});
+  $routeProvider.when('/scroll',    {templateUrl: "partials/scroll.html"}); 
+  $routeProvider.when('/toggle',    {templateUrl: "partials/toggle.html"}); 
+  $routeProvider.when('/tabs',      {templateUrl: "partials/tabs.html"}); 
+  $routeProvider.when('/accordion', {templateUrl: "partials/accordion.html"}); 
+  $routeProvider.when('/overlay',   {templateUrl: "partials/overlay.html"}); 
+  $routeProvider.when('/forms',     {templateUrl: "partials/forms.html"});
+  $routeProvider.when('/carousel',  {templateUrl: "partials/carousel.html"});
+});
+
+app.service('analytics', [
+  '$rootScope', '$window', '$location', function($rootScope, $window, $location) {
+    var send = function(evt, data) {
+      ga('send', evt, data);
+    }
+  }
+]);
+
+app.directive( "carouselExampleItem", function($rootScope, $swipe){
+  return function(scope, element, attrs){
+      var startX = null;
+      var startY = null;
+      var endAction = "cancel";
+      var carouselId = element.parent().parent().attr("id");
+
+      var translateAndRotate = function(x, y, z, deg){
+        element[0].style["-webkit-transform"] = "translate3d("+x+"px,"+ y +"px," + z + "px) rotate("+ deg +"deg)";
+        element[0].style["-moz-transform"] = "translate3d("+x+"px," + y +"px," + z + "px) rotate("+ deg +"deg)";
+        element[0].style["-ms-transform"] = "translate3d("+x+"px," + y + "px," + z + "px) rotate("+ deg +"deg)";
+        element[0].style["-o-transform"] = "translate3d("+x+"px," + y  + "px," + z + "px) rotate("+ deg +"deg)";
+        element[0].style["transform"] = "translate3d("+x+"px," + y + "px," + z + "px) rotate("+ deg +"deg)";
+      }
+
+      $swipe.bind(element, {
+        start: function(coords) {
+          endAction = null;
+          startX = coords.x;
+          startY = coords.y;
+        },
+
+        cancel: function(e) {
+          endAction = null;
+          translateAndRotate(0, 0, 0, 0);
+          e.stopPropagation();
+        },
+
+        end: function(coords, e) {
+          if (endAction == "prev") {
+            $rootScope.carouselPrev(carouselId);
+          } else if (endAction == "next") {
+            $rootScope.carouselNext(carouselId);
+          }
+          translateAndRotate(0, 0, 0, 0);
+          e.stopPropagation();
+        },
+
+        move: function(coords) {
+          if( startX != null) {
+            var deltaX = coords.x - startX;
+            var deltaXRatio = deltaX / element[0].clientWidth;
+            if (deltaXRatio > 0.3) {
+              endAction = "next";
+            } else if (deltaXRatio < -0.3){
+              endAction = "prev";
+            } else {
+              endAction = null;
+            }
+            translateAndRotate(deltaXRatio * 200, 0, 0, deltaXRatio * 15);
+          }
+        }
+      });
+    }
 });
